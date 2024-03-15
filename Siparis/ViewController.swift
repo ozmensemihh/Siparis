54e5cb845d640d7e4c10e17a56ddc95b5c84cf3f
semih <ozmensemih90@gmail.com>
2024-03-15 03:34:31 +0300

    Initial Commit

---
+++ Siparis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Siparis
+//
+//  Created by Dijitalvarliklar on 15.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
